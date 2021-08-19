1649cfa5e958ed7333c4d6a99d429bf015e9b2cd
stuartelimu <stuartelimu@gmail.com>
2021-08-19 04:39:57 +0300

    update do settings file

--- src/settings/do.py
+++ src/settings/do.py
@@ -16,6 +16,8 @@ DEBUG = env('DEBUG')
 
 SECRET_KEY = env('SECRET_KEY')
 
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
